c07cca38eedc21b63315c2af9a897a2e680280a4
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2015-11-27 15:29:15 +0800

    update

--- libs/requireJs/1_Pluralsight_requireJs/source/4-requirejs-javascript-dependency-injection-m4-exercise-files/After/src/js/main.js
+++
@@ -1,9 +0,0 @@
-require.config({
-    paths: {
-        jquery: 'jquery-2.1.1.min'
-    }
-});
-
-require(["app"], function (app) {
-    app.init();
-});
--- libs/requireJs/1_Pluralsight_requireJs/source/7-requirejs-javascript-dependency-injection-m7-exercise-files/After/src/js/require-config.js
+++
@@ -1,7 +0,0 @@
-require.config({
-    paths: {
-        jquery: 'jquery-2.1.1.min',
-        templates: '../templates',
-        hbs: '../../node_modules/require-handlebars-plugin/hbs'
-    }
-});
