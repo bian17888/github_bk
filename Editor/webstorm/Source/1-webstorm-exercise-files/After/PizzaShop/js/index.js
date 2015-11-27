f29ed0947b2248322ab6536f92a01378acff2c7d
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2015-11-27 15:36:28 +0800

    update

--- Editor/webstorm/Source/1-webstorm-exercise-files/After/PizzaShop/js/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Shawn on 9/27/2014.
- */
-(function () {
-
-    // TODO: Stop using alert
-    alert("code is running!");
-
-})();
--- Editor/webstorm/Source/3-webstorm-exercise-files/after/PizzaShop/js/animal.js
+++
@@ -1,8 +0,0 @@
-// animal.js
-var Animal = function () {
-  this.name = "";
-  this.legCount = 0;
-  this.feed  = function (food) {
-    console.log(this.name + " fed " + food);
-  };
-};
