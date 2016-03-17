21b98afa21aa8bba499ded5ef6af9d342ce2e248
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2016-03-17 10:45:35 +0800

    update

---
+++ angularjs/1_articles/base/baseController.js
@@ -0,0 +1,13 @@
+/**
+ * 参见文章 : [Angular浅入深出系列 - 基础知识] http://www.atatech.org/articles/46531
+ */
+
+var baseModule = angular.module('baseApp', []);
+
+/**
+ * 控制器 : message01Ctrl , message02Ctrl
+ * @desc 用于演示 作用域隔离 与 全局作用域
+ */
+baseModule.controller('expression', function ($scope) {
+
+});
