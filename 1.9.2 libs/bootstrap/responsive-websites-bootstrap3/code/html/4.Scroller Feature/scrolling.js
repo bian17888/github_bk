216d31a8a34c2c9fd622049a5feb12c0f126d7a3
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2016-07-06 19:58:42 +0800

    bootstrap : Scrolling

---
+++ 1.9.2 libs/bootstrap/responsive-websites-bootstrap3/code/html/4.Scroller Feature/scrolling.js
@@ -0,0 +1,26 @@
+/**
+ * @fileOverview
+ * @author bian17888 16/7/6 16:16
+ */
+
+$(function(){
+	$(window).on('load resize', function(){
+		$('.fill-screen').css('height', window.innerHeight);
+	})
+
+	$('body').scrollspy({
+		target : '.navbar',
+		offset : 110
+	})
+
+	// smooth scrolling
+	$('.nav a, .down-button a').bind('click', function () {
+		$('html, body').stop().animate({
+			scrollTop: $($(this).attr('href')).offset().top - 110
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+
+	$.stellar();
+
+})
