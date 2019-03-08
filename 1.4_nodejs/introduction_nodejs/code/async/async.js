60997b10dda23d3f20fae8f37f8ae916f79e4cc0
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2019-03-08 18:32:24 +0800

    async.js

--- 1.4_nodejs/introduction_nodejs/code/async/async.js
+++ 1.4_nodejs/introduction_nodejs/code/async/async.js
@@ -1,9 +1,25 @@
 const maxTime = 1000;
 
-const evenDouble = function(num, callback) {};
+const evenDouble = function(num, callback) {
+  if (num % 2) {
+    setTimeout(function() {
+      callback(new Error("Odd input"));
+    }, 1000);
+  } else {
+    setTimeout(function() {
+      callback(null, num * 2, 1000);
+    }, 1000);
+  }
+};
 
-const handleResults = function(err, results, time) {};
+const handleResults = function(err, results, time) {
+  if (err) {
+    console.log("Input error : " + err.message);
+  } else {
+    console.log("The results are : " + results + " ( " + time + " ms) ");
+  }
+};
 
-evenDouble(2, handleResults);
+evenDouble(5, handleResults);
 
 console.log("----------");
