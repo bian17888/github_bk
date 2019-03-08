a572e8eed553a4b066ac701bf695b1cba3f87b91
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2019-03-08 18:00:06 +0800

    introduction_nodejs init

---
+++ 1.4_nodejs/introduction_nodejs/code/async/async.js
@@ -0,0 +1,9 @@
+const maxTime = 1000;
+
+const evenDouble = function(num, callback) {};
+
+const handleResults = function(err, results, time) {};
+
+evenDouble(2, handleResults);
+
+console.log("----------");
