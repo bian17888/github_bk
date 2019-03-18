b8a4bfdd454000626ac1f039afa6a79d7b1a3350
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2019-03-18 11:43:45 +0800

    add websoket demo

--- 1.4_nodejs/introduction_nodejs/code/5_web/3_webSocket.js
+++ 1.4_nodejs/introduction_nodejs/code/5_web/3_webSocket.js
@@ -1,18 +1,33 @@
 const http = require("http");
+const fs = require("fs");
+const socketio = require("socket.io");
 
 const port = process.env.PORT;
 const ip = process.env.IP;
 
-http
-  .createServer(function(req, res) {
-    res.writeHead(200, { "Content-type": "text/plain" });
-    if (req.url === "/file.txt") {
-      fs.createReadStream(__dirname + "/file.txt").pipe(res);
-    } else {
-      res.end("Hello World!");
-    }
-  })
-  .listen(port, ip);
+const app = http.createServer(handler);
+const io = socketio(app);
+io.on("connection", socket => {
+  setInterval(() => {
+    let timestamp = Date.now();
+    socket.emit("timer", timestamp);
+  }, 2000);
+  socket.on("submit", data => {
+    console.log("Submitted : " + data);
+  });
+});
 
-console.log("Server is running !");
-console.log("port - " + port + " ip - " + ip);
+app.listen(port, ip);
+
+//////////////////////////////////////////////////
+
+function handler(req, res) {
+  res.writeHead(200, { "Content-type": "text/plain" });
+  if (req.url === "/file.txt") {
+    fs.createReadStream(__dirname + "/file.txt").pipe(res);
+  } else {
+    res.end("Hello World!");
+  }
+}
+
+console.log("Port - " + port + " Ip - " + ip);
